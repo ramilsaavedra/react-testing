fd5259696e52e873c11a57dc32c31a444f54c179
ramilsaavedra <ramilsmcl@gmail.com>
2024-02-17 22:06:33 +0800

    feat: test for showing users

--- src/components/UserList.test.tsx
+++ src/components/UserList.test.tsx
@@ -1,13 +1,38 @@
 import { screen, render } from '@testing-library/react';
 import UserList from './UserList.tsx';
+import UserContext, { UserProps } from '../context/userContext.tsx';
+
+function renderUserList(data: UserProps[]) {
+  render(
+    <UserContext.Provider value={{ users: data, handleUser: () => {} }}>
+      <UserList />
+    </UserContext.Provider>,
+  );
+}
 
 test('show no users when users is empty', () => {
-  const { container } = render(<UserList />);
-
-  const usersList = container.querySelector('div > div');
+  renderUserList([]);
 
   const noUsers = screen.getByText(/no users/i);
 
   expect(noUsers).toBeInTheDocument();
-  expect(usersList).toBeInTheDocument();
+});
+
+test('show the 2 users name and email', () => {
+  const mockData = [
+    {
+      name: 'jet',
+      email: 'jet@jet.com',
+    },
+    {
+      name: 'ruther',
+      email: 'ruther@email.com',
+    },
+  ];
+
+  renderUserList(mockData);
+
+  const userItem = screen.getAllByRole('listitem');
+
+  expect(userItem).toHaveLength(2);
 });
